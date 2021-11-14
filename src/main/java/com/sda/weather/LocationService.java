5106901045e63f04bc2e05b7aeaea4f486707d6d
Malgorzata Rywalska <gosia.rywalska@gmail.com>
2021-11-14 02:26:15 +0100

    HomeWork

--- src/main/java/com/sda/weather/LocationService.java
+++ src/main/java/com/sda/weather/LocationService.java
@@ -10,5 +10,24 @@ import java.util.regex.Pattern;
 public class LocationService {
 
     private final LocationRepository locationRepository;
+    private final ObjectMapper objectMapper;
+
+    public Location addNewLocation(String city, String region, String country, String longitude, String latitude) {
+
+        Pattern patternLatitude = Pattern.compile("[-90 -90]");
+        Pattern patternLongitude = Pattern.compile("[-180-180]");
+
+        Matcher matcherlongitude = patternLongitude.matcher(longitude);
+        Matcher matcherlatitude = patternLatitude.matcher(latitude);
+
+
+        if (city == null || city.isBlank() || country == null || country.isBlank()){
+            throw new IllegalArgumentException("Your location and coutry can't be empty!! ");
+        }else if (!matcherlatitude.matches() || !matcherlongitude.matches()){
+            throw new IllegalArgumentException("You gave me the wrong coordinates :( ");
+        }
+
+        return null;
+    }
 
 }
